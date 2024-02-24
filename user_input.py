ac0cbec7c995d40fbd3559ea88f2a607975303e6
Eric Kiverenge <avwengae@gmail.com>
2024-02-24 17:27:14 +0300

    Initial Commit

---
+++ user_input.py
@@ -0,0 +1,13 @@
+#Ask the User for  their name
+name = input("Enter your name: ")
+
+#Ask the User for their age
+age = input("Enter your age: ")
+
+#Ask the User for their location
+location = input("Enter your location: ")
+
+#Printing a personalised message
+message = "Hello " + name + ", I hope you are good. Wow you are " + age + " years old and living in " + location + ". Thank you for your feedback."
+print(message)
+
